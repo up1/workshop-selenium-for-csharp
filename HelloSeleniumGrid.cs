adfb8fbd71ffaf4b22dab794cb8129665986ed26
somkiat <somkiat.p@gmail.com>
2016-12-12 13:28:27 +0700

    Hello selenium grid

---
+++ HelloSeleniumGrid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace demo_ui_test
+{
+	[TestFixture]
+	public class HelloSeleniumGrid
+	{
+		IWebDriver webdriver;
+
+		[SetUp]
+		public void setup()
+		{
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities = DesiredCapabilities.Chrome();
+			capabilities.SetCapability(CapabilityType.BrowserName, "chrome");
+			capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Mac));
+
+			webdriver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capabilities);
+			webdriver.Manage().Window.Maximize();
+		}
+
+		[TearDown]
+		public void teardown()
+		{
+			webdriver.Quit();
+		}
+
+		[Test]
+		public void hello()
+		{
+			webdriver.Navigate().GoToUrl("http://www.google.com");
+		}
+			
+	}
+}
